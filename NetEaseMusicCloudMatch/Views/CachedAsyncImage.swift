bb82e8c5b504279cb52aafcd158cf267f0b44199
zhoujinyi <zhioj3i@163.com>
2024-11-20 02:00:52 +0800

    update: 图片加载时增加loading图

--- NetEaseMusicCloudMatch/Views/CachedAsyncImage.swift
+++ NetEaseMusicCloudMatch/Views/CachedAsyncImage.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct CachedAsyncImage: View {
     let url: URL?
     @State private var image: NSImage?
+    @State private var isLoading = false
     
     var body: some View {
         Group {
@@ -11,20 +12,32 @@ struct CachedAsyncImage: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
             } else {
-                Color.gray
-                    .onAppear {
-                        loadImage()
+                ZStack {
+                    Color.gray.opacity(0.1)
+                    if isLoading {
+                        ProgressView()
+                            .scaleEffect(0.4)
+                            .progressViewStyle(CircularProgressViewStyle())
                     }
+                }
+                .onAppear {
+                    isLoading = true
+                    loadImage()
+                }
             }
         }
     }
     
     private func loadImage() {
-        guard let url = url else { return }
+        guard let url = url else {
+            isLoading = false
+            return
+        }
         
         // 先从缓存中查找
         if let cachedImage = ImageCache.shared.get(url.absoluteString) {
             self.image = cachedImage
+            isLoading = false
             return
         }
         
@@ -35,6 +48,11 @@ struct CachedAsyncImage: View {
                     // 保存到缓存
                     ImageCache.shared.set(downloadedImage, for: url.absoluteString)
                     self.image = downloadedImage
+                    self.isLoading = false
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.isLoading = false
                 }
             }
         }.resume()
